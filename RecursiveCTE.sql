b91002b937baf070034ca724b058eba511e8941a
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2025-08-06 12:52:24 +0530

    Update RecursiveCTE.sql

--- RecursiveCTE.sql
+++ RecursiveCTE.sql
@@ -9,3 +9,18 @@ with recursive cte as (
 --recursive condition
 
 -- in sql we do recursion in toop down manner where we start from top and let condition follow to bootom
+
+
+
+-- table of two
+with recursive two_mulitple as(
+  select 2 as n --base condition
+  union all 
+  select n+2 from cte where n < 20 -- recursice condition  with reference to base condition
+)
+
+
+-- usecases :
+  -- finding heirarchial order 
+  -- finding routes
+  -- finding boss / parent
